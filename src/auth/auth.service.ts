d9d0a920e6d0a52b316e69cf4fca36bacaa4b37b
sdarnadeem <sdarnadeem@gmail.com>
2023-02-24 12:33:24 +0530

    add sign in handler

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,7 +31,32 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return 'Hello from  Login';
+  async signin(dto: AuthDto) {
+    // find the user by email
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // if user does not exist throw exception
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    // compare password
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    // if password is incorrect throw exception
+
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    delete user.hash;
+    // return the user
+    return user;
   }
 }
